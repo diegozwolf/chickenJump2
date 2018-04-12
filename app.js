961aae65c362aa5a65f636162f10b99cef1f687b
Diego Zuluaga <diegozuluaga88@gmail.com>
2018-04-12 16:11:07 -0500

    change methods

--- app.js
+++ app.js
@@ -1,70 +1,73 @@
-var cvs = getElementById('canvas')
-var ctx = cvs.getContext("2d");
-var horizon = cvs.height - 60
-var score = 0;
-var obstacles = []
-obstacles[0] = {
-  x: cvs.width,
-  y: horizon
-}
-var chicken = new Image('assets/chicken2.png', 100, horizon,cvs)
-var obstacleSpeed = 5
+$(document).ready(function(){
+  var cvs = document.getElementById('canvas')
+  var ctx = cvs.getContext('2d')
+  cvs.addEventListener('onload', draw)
+  var horizon = cvs.height - 60
+  var score = 0
+  var obstacles = []
+  obstacles[0] = {
+    x: cvs.width,
+    y: horizon
+  }
+  var chicken = new Image('assets/chicken2.png', 100, horizon, cvs)
+  var obstacleSpeed = 5
 
-var flame
+  var flame
 
 
-function draw () {
-  drawHorizon()
-  chicken.update(horizon)
-  handleObstacles()
-}
+  function draw () {
+    drawHorizon()
+    chicken.update(horizon)
+    handleObstacles()
+    requestAnimationFrame(draw)
+  }
 
-function drawHorizon(){
-  ctx.beginPath();
-  ctx.moveTo(0, horizon);
-  ctx.lineTo(cvs.width, horizon);
-  ctx.stroke();
+  function drawHorizon () {
+    ctx.beginPath()
+    ctx.moveTo(0, horizon)
+    ctx.lineTo(cvs.width, horizon)
+    ctx.stroke()
 
-  ctx.fillStyle = '#000'
-  ctx.font = '24px Verdana'
-  ctx.fillText('Score : ' + score, 10, cvs.width-30)
-  requestAnimationFrame(drawHorizon)
-}
+    ctx.fillStyle = '#000'
+    ctx.font = '24px Verdana'
+    ctx.fillText('Score : ' + score, 10, cvs.width - 30)
+    requestAnimationFrame(drawHorizon)
+  }
 
-function handleObstacles () {
-  for (var i = 0; i < obstacles.length; i++) {
-    var obsX = ctx.getImageData.x
-    obstacles[i].updateImage(flame, cvs, horizon, obstacleSpeed)
-    obstacles[i].drawElement(flame, obsX, horizon, cvs )
+  function handleObstacles () {
+    for (var i = 0; i < obstacles.length; i++) {
+      var obsX = ctx.getImageData.x
+      obstacles[i].updateImage(flame, cvs, horizon, obstacleSpeed)
+      obstacles[i].drawElement(flame, obsX, horizon, cvs)
 
-    if (obstacles[i].hit(chicken)) {
-      endGame()
-    }
-    else {
-      score += 5
-    }
+      if (obstacles[i].hit(chicken)) {
+        endGame()
+      }
+      else {
+        newObstacles()
+        score += 5
+      }
 
-    if (!obstacles[i].onScreen) {
-      obstacles.splice(i, 1)
+      if (!obstacles[i].onScreen) {
+        obstacles.splice(i, 1)
+      }
     }
   }
-}
 
-function  newObstacles () {
-  flame = new Image('assets/flame2.png', cvs.width, horizon,cvs)
-}
-
-function keyPressed () {
-  if ((keyCode === UP_ARROW || keyCode === 32)&& chicken.onGround){
-    chicken.jump()
+  function newObstacles () {
+    flame = new Image('assets/flame2.png', cvs.width, horizon, cvs)
   }
-}
-function handleLevel(level) {
 
-}
+  document.addEventListener('UP_ARROW', keyPressed)
+  function keyPressed () {
+    if ((keyCode === UP_ARROW || keyCode === 32) && chicken.onGround){
+      chicken.jump()
+    }
+  }
 
-function endGame () {
-  ctx.fillStyle = '#000'
-  ctx.font = '24px Verdana'
-  ctx.fillText('press spacerbar to restart : ', cvs.width / 2, cvs.height / 2)
-}
+  function endGame () {
+    ctx.fillStyle = '#000'
+    ctx.font = '24px Verdana'
+    ctx.fillText('press spacerbar to restart : ', cvs.width / 2, cvs.height / 2)
+  }
+})
--- image.js
+++ image.js
@@ -1,45 +1,48 @@
-function Image(url, x, y, cvs){
-  this.image = document.createElement('IMG');
-  this.image.setAttribute("src", url)
-  this.image.setAttribute("x", x)
-  this.image.setAttribute("y",y)
-  document.body.appendChild(this.image)
-
-  this.onOnground = true
-  this.yVelocity = 0
-  this.speed = 1
-}
-
-
-function updateImage(img, cvs, horizon, speed) {
-  var nextBottom = horizon + this.yVelocity
-  if (this.img.src === 'assets/chicken2.png') {
-    if ( this.img.y <= horizon  && nextBottom >= horizon) {
-      this.onGround = true
-      img.y = horizon - this.yVelocity
-      this.yVelocity = 0
-    } else if(horizon - this.img.y > 1){
-      this.yVelocity += speed
-      this.onGround = false
-    }
-  } else {
-    this.onScreen = (this.image.x > (cvs.width -(cvs.width-1)) && this.image.x + this.image.width < cvs.width)
-    this.image.x -= speed
+$(document).ready(function(){
+  function Image (url, x, y, cvs) {
+    this.image = document.createElement('IMG')
+    this.image.setAttribute("src", url)
+    this.image.setAttribute("x", x)
+    this.image.setAttribute("y",y)
+    document.body.appendChild(this.image)
+    this.onOnground = true
+    this.yVelocity = 0
+    this.speed = 1
   }
-}
 
-function drawElement(img, x,y, cvs){
-  ctx = cvs.getContext('2d')
-  ctx.drawImage(img,x,y)
-  ctx.fillStyle = "#fff"
-  ctx.fillRect(0,0,cvs.width,cvs.height)
-  ctx.clearRect(0,0,cvs.width,cvs.height)
-}
 
-function hit(img, obs){
-  return (obs.x <= image.x && obs.x >= img.x - img.width)
-}
+  function updateImage(img, cvs, horizon, speed) {
+    var nextBottom = horizon + this.yVelocity
+    if (this.img.src === 'assets/chicken2.png') {
+      if ( this.img.y <= horizon  && nextBottom >= horizon) {
+        this.onGround = true
+        img.y = horizon - this.yVelocity
+        this.yVelocity = 0
+      } else if(horizon - this.img.y > 1){
+        this.yVelocity += speed
+        this.onGround = false
+      }
+    } else {
+      this.onScreen = (this.image.x > (cvs.width -(cvs.width-1)) && this.image.x + this.image.width < cvs.width)
+      this.image.x -= speed
+    }
+  }
 
-function jump(){
-  this.yVelocity = -(image.width / 2) * 0.75
-}
+  function drawElement(img, x,y, cvs){
+    ctx = cvs.getContext('2d')
+    ctx.drawImage(img,x,y)
+    ctx.fillStyle = "#fff"
+    ctx.fillRect(0,0,cvs.width,cvs.height)
+    ctx.clearRect(0,0,cvs.width,cvs.height)
+    requestAnimationFrame(drawElement)
+  }
+
+  function hit (img, obs){
+    return (obs.x <= image.x && obs.x >= img.x - img.width)
+  }
+
+  function jump(){
+    this.yVelocity = -(image.width / 2) * 0.75
+  }
+
+})
